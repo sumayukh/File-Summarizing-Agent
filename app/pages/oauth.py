fe0d03fcf1d96205301a9acd23fa75fb794e5418
Sumayukh <sumayukh.sinha@yahoo.com>
2026-03-05 23:18:50 +0530

    FIX:Revert ui change in oauth.py

--- app/pages/oauth.py
+++ app/pages/oauth.py
@@ -9,7 +9,7 @@ def render_oauth():
             query_params = st.query_params
             if "code" not in query_params:
                 auth_url = login()
-                st.link_button("Login with Google", auth_url)
+                st.markdown(f'<a href="{auth_url}" target="_top">Login with Google</a>', unsafe_allow_html=True)
             else:
                 code = query_params.get("code")
                 credentials = oauth_callback(code)
